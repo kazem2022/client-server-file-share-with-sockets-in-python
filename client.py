e6342a403afe4bf379e4de70ad711c0a92646944
kazem <k_jebraeeli@yahoo.com>
2023-09-12 18:26:38 +0330

    before buffer

--- client.py
+++ client.py
@@ -2,7 +2,7 @@ import socket
 import os
 
 ip = "localhost"
-port = 8092
+port = 8080
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
     client.connect((ip, port))
     message = client.recv(1024).decode()
@@ -11,11 +11,11 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
     print(message2)
     file_name = input()
     request = client.sendall(file_name.encode())
-    requested_file = client.recv(1024)
+    requested_file = client.recv(4072)
     if requested_file == b"File not found!":
         print(requested_file.decode()) 
     else:
-        with open(f"./client-files/{file_name}", "wb") as file:
+        with open(f"../client-files/{file_name}", "wb") as file:
             file.write(requested_file)
         print("file downloaded from server.")
         
--- server.py
+++ server.py
@@ -2,34 +2,37 @@ import socket
 import os
 import threading
 
-ip = "localhost"
-port = 8092
+ip = "127.0.0.1"
+port = 8080
 
 def client_function(client):
     
-    files = os.listdir("server-files")
+    files = os.listdir("../server-files")
     files_path = "\n".join(files)
     
     client.send(files_path.encode())
     
-    with open("./server-files/a.txt", "rb") as file:
-        data2 = file.read(1024)
-        client.send("\nPlease input the desired file name:\n ".encode())
-        
-        requested_file = client.recv(1024).decode()
-        if requested_file in files:
-            with open(f"./server-files/{requested_file}", "rb") as file:
-                request_answer = client.send(file.read(1024))
-        else:
-            client.send(b"File not found!")
+    # with open("../server-files/a.txt", "rb") as file:
+    #     data2 = file.read(4072)
+    client.send("\nPlease input the desired file name:\n ".encode())
+    
+    requested_file = client.recv(1024).decode()
+    if requested_file in files:
+        with open(f"../server-files/{requested_file}", "rb") as file:
+            request_answer = client.send(file.read(1024))
+        print(request_answer)
+    else:
+        client.send(b"File not found!")
 clients = []
 while True:
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
         server.bind((ip, port))
         server.listen(5)
         print(f"server is listening on address:{ip,port}...")
+        
         client, address = server.accept() 
         print(f"client with address: {address} connected")
+        # print("client:", client)
         clients.append(client)
         threading.Thread(target=client_function, args=(client,)).start()
-       
+    
