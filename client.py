53f8ca61bef1b48370dae33be2978652bb5b0358
kazem <k_jebraeeli@yahoo.com>
2023-09-13 21:06:05 +0330

    clean code approach added

--- client.py
+++ client.py
@@ -1,94 +1,96 @@
+"""Importing library"""
 import socket
 import os
 from tqdm import tqdm
 
 ip = "localhost"
-port = 8086
+port = 8092
 buffer_size = 512 #Bytes => KB
 
 def receive_file():
+    """This function receive data from server"""
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
         client.connect((ip, port))
-        
         client.send("down".encode())
-        
+        # Receive files list and print
         message = client.recv(buffer_size).decode()
         print(message)
         message2 = client.recv(buffer_size).decode()
         print(message2)
         file_name = input()
-        request = client.sendall(file_name.encode())
+        client.sendall(file_name.encode())
         file_size = int(client.recv(buffer_size).decode())
-        # print("file_size:", file_size)
         requested_file = client.recv(buffer_size)
-        progress = tqdm(range(file_size), f"receiving {file_name}", unit="B", unit_scale=True, unit_divisor=buffer_size)
+        
+        # Make a progress to drawing progress-bar
+        progress = tqdm(range(file_size), f"receiving {file_name}",
+                        unit="B", unit_scale=True, unit_divisor=buffer_size)
+        
         if requested_file == b"File not found!":
             print(requested_file.decode()) 
         else:
+            # File receiving cycle
             with open(f"../client-files/{file_name}", "wb") as file:
                 while True:
                     data = client.recv(buffer_size)
-                    if data == b"file sent!":
+                    if data == b"File sent!":
                         print("***************")
                         break
                     file.write(data)
                     progress.update(len(data))
-                                         
+                    
             print("file downloaded from server.")
 
+
 def send_file():
+    """This function send data to server"""
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
         client.connect((ip, port))
         print("connected to the server.")
         client.send("up".encode())
+        # Showing client file list
         files = os.listdir("../client-files")
         files_path = "\n".join(files)
         print(files_path)
         file_name = input("select your desire file...\n")
-                   
+        
+        # Error handling
         try:
             path = f"../client-files/{file_name}"
             file_size = os.stat(path).st_size
             client.send(str(file_size).encode())
             client.send(file_name.encode())
-            progress = tqdm(range(file_size), f"sendig {file_name}", unit="B", unit_scale=True, unit_divisor=buffer_size)
             
+            # Make a progress to drawing progress-bar
+            progress = tqdm(range(file_size), f"sendig {file_name}", unit="B",
+                            unit_scale=True, unit_divisor=buffer_size)
+            
+            # Sending process to sending file to the server
             with open(path, "rb") as file:
                 steps = int(file_size/buffer_size + 1)
                 for step in range(steps):
                     data = file.read(buffer_size)
                     client.send(data)
                     progress.update(len(data))
-                client.send(b"file sent!")
-                print("file sent!!!")
+                client.send(b"File sent!")
+                print("File sent!!!")
                 
         except FileNotFoundError:
             print("File not found!")
 
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
 def loader():
-    answer = input("do you wanna upload a file or download from server?        up/down :  ")
+    """This function is handling client works"""
+    answer = input("Do you want to upload a file or download it from the server?\n        up / down :  ")
     if answer.lower() == "up":
         send_file()
     elif answer.lower() == "down":
         receive_file()
     else:
-        print("type your response is incorrect!!")
-        
-
+        print("Type your response is incorrect!!")
 if __name__ == "__main__":
     loader()
-    
-   
+
 
 
     
--- server.py
+++ server.py
@@ -1,47 +1,48 @@
+"""Importing library"""
 import socket
 import os
 import threading
 from tqdm import tqdm
 
+"""Initial data"""
 ip = "127.0.0.1"
-port = 8086
+port = 8092
 buffer_size = 512 #Bytes => KB
 
 def send_file(client):
-    
+    """This function sends data to clients"""
     files = os.listdir("../server-files")
     files_path = "\n".join(files)
     client.send(files_path.encode())
     client.send("\nPlease input the desired file name:\n ".encode())
-    
     requested_file = client.recv(buffer_size).decode()
+    
+    # Error handling
     try:
         file_path = f"../server-files/{requested_file}"
         file_size = os.stat(file_path).st_size
         client.send(str(file_size).encode())
         progress = tqdm(range(file_size), f"sendig {requested_file}", unit="B", unit_scale=True, unit_divisor=buffer_size)
-        
         with open(file_path, "rb") as file:
             steps = int(file_size/buffer_size + 1)
             for step in range(steps):
                 data = file.read(buffer_size)
                 client.send(data)
                 progress.update(len(data))
-            client.send(b"file sent!")
-            print("file sent!!!")
+            client.send(b"File sent!")
+            print("File sent!!!")
             
     except FileNotFoundError:
         client.send(b"File not found!")
 
 
 def receive_file(client):
-   
+    """This function receives data from clients."""
     file_size = int(client.recv(buffer_size).decode())
     print("file_size:", file_size)
     file_name = client.recv(buffer_size).decode()
-    
-    progress = tqdm(range(file_size), f"receiving {file_name}", unit="B", unit_scale=True, unit_divisor=buffer_size)
-    
+    progress = tqdm(range(file_size), f"receiving {file_name}",
+                    unit="B", unit_scale=True, unit_divisor=buffer_size)
     with open(f"../server-files/{file_name}", "wb") as file:
         while True:
             data = client.recv(buffer_size)
@@ -50,20 +51,19 @@ def receive_file(client):
                 break
             file.write(data)
             progress.update(len(data))
-                                    
-    print("file downloaded from server.") 
+    print("File downloaded from server.") 
 
 
-
+"""Server main loop for listening"""
 while True:
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
         server.bind((ip, port))
         server.listen(5)
-        print(f"server is listening on address:{ip,port}...")
-        
+        print(f"Server is listening on address:{ip,port}...")
         client, address = server.accept() 
-        print(f"client with address: {address} connected")
+        print(f"Client with address: {address} connected")
         
+        # Threading system
         answer = client.recv(buffer_size).decode()
         if answer == "up":
             client_thread = threading.Thread(target=receive_file, args=(client,))
@@ -71,4 +71,4 @@ while True:
         if answer == "down":
             client_thread = threading.Thread(target=send_file, args=(client,))
             client_thread.start()
-    
+            
